7187bf9d448d8895c107f6a7067d0096239bd53d
Nik Vasilzhenko <polypteroos@gmail.com>
2019-02-06 12:15:07 +0300

    fix

--- js/jquery.init.js
+++ js/jquery.init.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
 		dots: false,
 		infinite: false
 	});
+	$('.js-main-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		infinite: false
+	});
 	
              
     $('#js-carusel').slick({
